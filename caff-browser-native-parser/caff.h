6a015ed7bce1cc7462d33bcd2cce90a3089e8898
Belcsik Bence <belcsik.bebe09@gmail.com>
2021-11-07 17:37:43 +0100

    JSON writer function added, ofstream revision needed!

--- caff-browser-native-parser/caff.h
+++ caff-browser-native-parser/caff.h
@@ -3,6 +3,134 @@
 
 #include "util.h"
 #include "ciff.h"
+#include <fstream>
+
+class caff {
+public:
+    // header
+    uint8_t header_id;
+    uint64_t header_length;
+    string header_magic;
+    uint64_t header_size;
+    uint64_t num_anim;
+    // credits
+    uint8_t credit_id;
+    uint64_t credit_length;
+    uint16_t year;
+    uint8_t month;
+    uint8_t day;
+    uint8_t hour;
+    uint8_t minute;
+    uint64_t creator_size;
+    string creator;
+    // animation
+    vector<uint8_t> animation_ids;
+    vector<uint64_t> animation_lengths;
+    vector<uint64_t> animation_durations;
+    vector<ciff> images;
+
+    caff(uint8_t headerId, uint64_t headerLength, const string &headerMagic, uint64_t headerSize, uint64_t numAnim, uint8_t creditId,
+         uint64_t creditLength, uint16_t p_year, uint8_t p_month, uint8_t p_day, uint8_t p_hour, uint8_t p_minute,
+         uint64_t creatorSize, const string &p_creator, const vector<uint8_t> &animationIds,
+         const vector<uint64_t> &animationLengths, const vector<uint64_t> &animationDurations, const vector<ciff> &ciffImgs) {
+            header_id = headerId;
+            header_length = headerLength;
+            header_magic = headerMagic;
+            header_size = headerSize;
+            num_anim = numAnim;
+            credit_id = creditId;
+            credit_length = creditLength;
+            year = p_year;
+            month = p_month;
+            day = p_day;
+            hour = p_hour;
+            minute = p_minute;
+            creator_size = creatorSize;
+            creator = p_creator;
+            animation_ids = animationIds;
+            animation_lengths = animationLengths;
+            animation_durations = animationDurations;
+            images = ciffImgs;
+    }
+
+    void writeToJSON(string fname) {
+        ofstream of;
+        of.open(fname);
+        if(of.is_open()) {
+            of << "{\n";
+            of << "\t\"header\":{\n";
+            of << "\t\t\"id\":" << header_id << "\n";
+            of << "\t\t\"length\":" << header_length << "\n";
+            of << "\t\t\"magic\":\"" << header_magic << "\"\n";
+            of << "\t\t\"header_size\":" << header_size << "\n";
+            of << "\t\t\"num_anim\":" << num_anim << "\n";
+            of << "\t}\n";
+            of << "\t\"credit\":{\n";
+            of << "\t\t\"id\":" << credit_id << "\n";
+            of << "\t\t\"length\":" << credit_length << "\n";
+            of << "\t\t\"year\":" << year << "\n";
+            of << "\t\t\"month\":" << month << "\n";
+            of << "\t\t\"day\":" << day << "\n";
+            of << "\t\t\"hour\":" << hour << "\n";
+            of << "\t\t\"minute\":" << minute << "\n";
+            of << "\t\t\"creator_length\":" << creator_size << "\n";
+            of << "\t\t\"creator\":\"" << creator << "\"\n";
+            of << "\t}\n";
+            of << "\t\"animation\":{\n";
+            of << "\t\t\"ids\":[";
+            for (int i = 0; i < animation_ids.size() - 1; i++) {
+                of << animation_ids[i] << ", ";
+            }
+            of << animation_ids[animation_ids.size() - 1] << "]\n";
+            of << "\t\t\"lengths\":[";
+            for (int i = 0; i < animation_lengths.size() - 1; i++) {
+                of << animation_lengths[i] << ", ";
+            }
+            of << animation_lengths[animation_lengths.size() - 1] << "]\n";
+            of << "\t\t\"ids\":[";
+            for (int i = 0; i < animation_durations.size() - 1; i++) {
+                of << animation_durations[i] << ", ";
+            }
+            of << animation_durations[animation_durations.size() - 1] << "]\n";
+            of << "\t\t\"CIFFs\":[\n";
+            for (int i = 0; i < images.size() - 1; i++) {
+                of << "\t\t\t{\n";
+                of << "\t\t\t\t\"magic\":\"" << images[i].magic << "\"\n";
+                of << "\t\t\t\t\"header_size\":" << images[i].header_size << "\n";
+                of << "\t\t\t\t\"content_size\":" << images[i].content_size << "\n";
+                of << "\t\t\t\t\"width\":" << images[i].width << "\n";
+                of << "\t\t\t\t\"height\":" << images[i].height << "\n";
+                of << "\t\t\t\t\"caption\":\"" << images[i].caption << "\"\n";
+                of << "\t\t\t\t\"tags\":[";
+                for (int i = 0; i < images[i].tags.size() - 1; i++) {
+                    of << "\"" << images[i].tags[i] << "\", ";
+                }
+                of << "\"" << images[i].tags[images[i].tags.size() - 1] << "\"]\n";
+                of << "\t\t\t}";
+            }
+            of << "\t\t\t{\n";
+            of << "\t\t\t\t\"magic\":\"" << images[images.size() - 1].magic << "\"\n";
+            of << "\t\t\t\t\"header_size\":" << images[images.size() - 1].header_size << "\n";
+            of << "\t\t\t\t\"content_size\":" << images[images.size() - 1].content_size << "\n";
+            of << "\t\t\t\t\"width\":" << images[images.size() - 1].width << "\n";
+            of << "\t\t\t\t\"height\":" << images[images.size() - 1].height << "\n";
+            of << "\t\t\t\t\"caption\":\"" << images[images.size() - 1].caption << "\"\n";
+            of << "\t\t\t\t\"tags\":[";
+            for (int i = 0; i < images[images.size() - 1].tags.size() - 1; i++) {
+                of << "\"" << images[images.size() - 1].tags[i] << "\", ";
+            }
+            of << "\"" << images[images.size() - 1].tags[images[images.size() - 1].tags.size() - 1] << "\"]\n";
+            of << "\t\t\t}";
+            of << "]\n";
+            of << "\t}\n";
+            of << "}";
+
+            of.close();
+        } else {
+            throw runtime_error("File is not open!");
+        }
+    }
+};
 
 void parse_caff(const char *buffer) {
     //CAFF Header
@@ -62,6 +190,11 @@ void parse_caff(const char *buffer) {
     printf("Creator: %s\n\n", creator.c_str());
 
     //CAFF Animation
+    vector<uint8_t> anIDs;
+    vector<uint64_t> anLens;
+    vector<uint64_t> anDurs;
+    vector<ciff> ciffImgs;
+
     printf("CAFF Animation:\n");
     uint64_t start = creator_end_index + 17;
     for (uint64_t image_number = 0; image_number < num_anim; image_number++) {
@@ -73,14 +206,24 @@ void parse_caff(const char *buffer) {
         uint64_t duration = parse_int_byte(buffer, creator_end_index + 9, 8);
 
         printf("Id: %d\n", animation_id);
+        anIDs.push_back(animation_id);
         printf("Length: %llu\n", animation_length);
+        anLens.push_back(animation_length);
         printf("Duration: %llu\n", duration);
+        anDurs.push_back(duration);
 
         string image_index = uint64_to_string(image_number);
 
-        parse_ciff(image_index, buffer, start);
+        ciff ciffObj = parse_ciff_with_return(image_index, buffer, start);
+        ciffImgs.push_back(ciffObj);
         start += animation_length + 9;
     }
+
+    caff caffObj(header_id, header_length, magic.c_str(), header_size, num_anim, credit_id, credit_length,
+                 year, month, day, hour, minute, creator_size, creator.c_str(), anIDs, anLens, anDurs, ciffImgs);
+
+    // kipróbálni!
+    caffObj.writeToJSON("../caffObj.json");
 }
 
 #endif //CAFF_BROWSER_NATIVE_PARSER_CAFF_H
--- caff-browser-native-parser/ciff.h
+++ caff-browser-native-parser/ciff.h
@@ -9,6 +9,47 @@
 
 using namespace std;
 
+class ciff {
+public:
+    string magic;
+    uint64_t header_size;
+    uint64_t content_size;
+    uint64_t width;
+    uint64_t height;
+    string caption;
+    vector<string> tags;
+
+    ciff(const string &p_magic, uint64_t headerSize, uint64_t contentSize, uint64_t p_width, uint64_t p_height,
+         const string &p_caption, const vector<string> &p_tags){
+        magic = p_magic;
+        header_size = headerSize;
+        content_size = contentSize;
+        width = p_width;
+        height = p_height;
+        caption = p_caption;
+        tags = p_tags;
+    }
+
+    void writeToJSON(string fname) {
+        ofstream of(fname);
+        of << "{\n";
+        of << "\t\"magic\":\""<< magic << "\"\n";
+        of << "\t\"header_size\":" << header_size << "\n";
+        of << "\t\"content_size\":" << content_size << "\n";
+        of << "\t\"width\":" << width << "\n";
+        of << "\t\"height\":" << height << "\n";
+        of << "\t\"caption\":\"" << caption << "\"\n";
+        of << "\t\"tags\":[";
+        for(int i = 0; i<tags.size()-1;i++) {
+            of << "\"" << tags[i] << "\", ";
+        }
+        of << "\"" << tags[tags.size()-1] << "\"]\n";
+        of << "}";
+
+        of.close();
+    }
+};
+
 pair<string, int> parse_caption(const char *buffer, uint64_t start_index, uint64_t max_end_index) {
     uint64_t caption_end_index = -1;
     for (uint64_t i = start_index; i <= max_end_index; i++) {
@@ -85,12 +126,52 @@ void parse_ciff(const string &file_name, const char *buffer, uint64_t start_inde
         cout << tag << ", ";
     }
 
+    ciff ciffObj(magic, header_size, content_size, width, height, caption, tags);
+
     unsigned char pixels[content_size];
     parse_rgb_pixels(buffer, start_index + header_size, content_size, pixels);
     convert_to_ppm(file_name, width, height, pixels);
 
     cout << endl << endl;
 
+}
+
+ciff parse_ciff_with_return(const string &file_name, const char *buffer, uint64_t start_index) {
+    string magic = parse_string_byte(buffer, start_index, 4);
+    if (magic != "CIFF") {
+        throw runtime_error("Invalid CIFF magic");
+    }
+    uint64_t header_size = parse_int_byte(buffer, start_index + 4, 8);
+    uint64_t content_size = parse_int_byte(buffer, start_index + 12, 8);
+    uint64_t width = parse_int_byte(buffer, start_index + 20, 8);
+    uint64_t height = parse_int_byte(buffer, start_index + 28, 8);
+    if (content_size != width * height * 3) {
+        throw runtime_error("Invalid CIFF content size");
+    }
+    pair<string, int> caption_pair = parse_caption(buffer, start_index + 36, start_index + header_size - 1);
+    string caption = caption_pair.first;
+    int caption_end_index = caption_pair.second;
+    vector<string> tags = parse_tags(buffer, caption_end_index + 1, start_index + header_size - 1);
+
+    printf("Header: %llu\n", header_size);
+    printf("Content Size: %llu\n", content_size);
+    printf("Width: %llu\n", width);
+    printf("Height: %llu\n", height);
+    printf("Caption: %s", caption.c_str());
+    printf("Tags: ");
+    for (auto &tag: tags) {
+        cout << tag << ", ";
+    }
+
+    ciff ciffObj(magic, header_size, content_size, width, height, caption, tags);
+
+    unsigned char pixels[content_size];
+    parse_rgb_pixels(buffer, start_index + header_size, content_size, pixels);
+    convert_to_ppm(file_name, width, height, pixels);
+
+    cout << endl << endl;
+
+    return ciffObj;
 }
 
 #endif //CAFF_BROWSER_NATIVE_PARSER_CIFF_H
