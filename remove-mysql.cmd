f4ed569f1287b87dd1221ba08c05b24302c30b5a
Rezsek Máté <rezsekmv@gmail.com>
2021-11-22 23:38:17 +0100

    dockerize database

---
+++ remove-mysql.cmd
@@ -0,0 +1,9 @@
+TITLE CaffBrowser MySql Server 
+
+ECHO stopping docker containers
+docker compose stop
+
+ECHO remove docker containers
+docker compose rm
+
+ECHO done
---
+++ start-mysql.cmd
@@ -0,0 +1,9 @@
+TITLE CaffBrowser MySql Server 
+
+ECHO building docker images
+docker compose build
+
+ECHO starting docker containers
+docker compose up
+
+ECHO done
