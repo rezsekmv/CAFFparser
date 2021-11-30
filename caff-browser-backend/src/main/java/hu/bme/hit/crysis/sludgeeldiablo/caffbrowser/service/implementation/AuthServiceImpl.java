fa0c19c0d7688d43be16f70d8493fea1d0432303
Bősz Norbert <norbert.bosz@gmail.com>
2021-11-30 23:30:53 +0100

    refresh token visszaadja a stringet 2

--- caff-browser-backend/src/main/java/hu/bme/hit/crysis/sludgeeldiablo/caffbrowser/service/implementation/AuthServiceImpl.java
+++ caff-browser-backend/src/main/java/hu/bme/hit/crysis/sludgeeldiablo/caffbrowser/service/implementation/AuthServiceImpl.java
@@ -3,12 +3,10 @@ package hu.bme.hit.crysis.sludgeeldiablo.caffbrowser.service.implementation;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import hu.bme.hit.crysis.sludgeeldiablo.caffbrowser.exception.CbTokenException;
 import hu.bme.hit.crysis.sludgeeldiablo.caffbrowser.model.User;
 import hu.bme.hit.crysis.sludgeeldiablo.caffbrowser.service.declaration.AuthService;
 import hu.bme.hit.crysis.sludgeeldiablo.caffbrowser.service.declaration.UserService;
-import hu.bme.hit.crysis.sludgeeldiablo.caffbrowser.util.ObjectMapperFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,15 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
-import static hu.bme.hit.crysis.sludgeeldiablo.caffbrowser.security.SecurityVariables.*;
+import static hu.bme.hit.crysis.sludgeeldiablo.caffbrowser.security.SecurityVariables.BEARER_TOKEN_START;
+import static hu.bme.hit.crysis.sludgeeldiablo.caffbrowser.security.SecurityVariables.ROLES_PARAMETER;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
 @Service
@@ -40,7 +35,6 @@ public class AuthServiceImpl implements AuthService {
     private Integer accessTokenExpiration;
 
     private final UserService userService;
-    private final ObjectMapper objectMapper = ObjectMapperFactory.createObjectMapper();
 
     @Override
     public String refreshToken(HttpServletRequest request, HttpServletResponse response) {
@@ -55,10 +49,7 @@ public class AuthServiceImpl implements AuthService {
                 DecodedJWT decodedJwt = JWT.require(algorithm).build().verify(refreshToken);
                 String username = decodedJwt.getSubject();
                 User user = userService.findByUsername(username);
-
-                Map<String, String> map = createTokenMap(request, user, algorithm);
-                valueIntoResponse(response, map);
-                return map.get(ACCESS_TOKEN_PARAMETER);
+                return createAccessToken(request, user, algorithm);
             } catch (Exception e) {
                 throw new CbTokenException(e.getMessage());
             }
@@ -74,17 +65,6 @@ public class AuthServiceImpl implements AuthService {
         if (!header.startsWith(BEARER_TOKEN_START)) {
             throw new CbTokenException("Authorization type must be Bearer token");
         }
-    }
-
-    private void valueIntoResponse(HttpServletResponse response, Map<String, String> map) throws IOException {
-        response.setContentType(APPLICATION_JSON_VALUE);
-        objectMapper.writeValue(response.getOutputStream(), map);
-    }
-
-    private HashMap<String, String> createTokenMap(HttpServletRequest request, User user, Algorithm algorithm) {
-        return new HashMap<String, String>() {{
-            put(ACCESS_TOKEN_PARAMETER, createAccessToken(request, user, algorithm));
-        }};
     }
 
     private String createAccessToken(HttpServletRequest request, User user, Algorithm algorithm) {
